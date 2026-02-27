e2305c2023e0fb1e87941e7e034cb6785d314e1d
Segni Mideksa <segnim21@gmail.com>
2026-02-27 13:05:11 +0300

    Start building

---
+++ FE-Capstone-project/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <div className="container mx-auto flex justify-between">
+        <h1 className="text-lg font-bold">Weather Dashboard</h1>
+
+        <div className="space-x-4">
+          <Link to="/" className="hover:underline">Home</Link>
+          <Link to="/about" className="hover:underline">About</Link>
+          <Link to="/contact" className="hover:underline">Contact</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
