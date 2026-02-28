6d07d5d323629afd675b4f8dafcfa2461009f9a5
Segni Mideksa <segnim21@gmail.com>
2026-02-28 14:02:40 +0300

    Add popular Cities in Africa

--- FE-Capstone-project/src/components/WeatherCard.jsx
+++ FE-Capstone-project/src/components/WeatherCard.jsx
@@ -1,38 +1,38 @@
 import { useEffect, useState } from "react";
 import ErrorMessage from "./ErrorMessage";
 
-function WeatherCard({ city }) {
+function WeatherCard({ city, setCity}) {
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
-  useEffect(() => {
-    const fetchWeather = async () => {
-      try {
-        setLoading(true);
-        setError(null);
+  const fetchWeather = async () => {
+  try {
+    setLoading(true);
+    setError(null);
 
-        const response = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
-        );
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    );
 
-        if (!response.ok) {
-          throw new Error("City not found");
-        }
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
 
-        const data = await response.json();
-        setWeather(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+    const data = await response.json();
+    setWeather(data);
+  } catch (err) {
+    setError(err.message);
+  } finally {
+    setLoading(false);
+  }
+};
 
-    fetchWeather();
-  }, [city]);
+useEffect(() => {
+  fetchWeather();
+}, [city]);
 
   if (loading) return <p className="text-center mt-10">Loading...</p>;
   if (error) return <ErrorMessage message={error} />;
@@ -79,7 +79,33 @@ function WeatherCard({ city }) {
           <p>{Math.round(weather.main.feels_like)}°C</p>
         </div>
       </div>
+      <div className="text-center mb-6">
+  <button
+    onClick={fetchWeather}
+    className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition"
+  >
+    🔄 Refresh
+  </button>
+</div>
+<div className="mt-8">
+  <h3 className="text-xl font-bold text-center mb-4">
+    🌍 Popular Cities
+  </h3>
+
+  <div className="flex flex-wrap justify-center gap-4">
+    {["Addis Ababa", "Abuja", "Cairo", "Dodoma", "Pretoria", "Nairobi"].map((c) => (
+      <button
+        key={c}
+        onClick={() => setCity(c)}
+        className="bg-gray-200 px-4 py-2 rounded-lg hover:bg-gray-300 transition"
+      >
+        {c}
+      </button>
+    ))}
+  </div>
+</div>
     </div>
+    
   );
 }
 
