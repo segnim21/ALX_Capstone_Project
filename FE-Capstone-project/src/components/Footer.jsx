b544a48b09e929dccea94488aee2d3a0f94e82d6
Segni Mideksa <segnim21@gmail.com>
2026-03-07 05:25:49 +0300

    Check for deployment is working by adding UI

--- FE-Capstone-project/src/components/Footer.jsx
+++ FE-Capstone-project/src/components/Footer.jsx
@@ -74,7 +74,7 @@ function Footer() {
             </div>
 
             <p className="mt-4 text-sm">
-              Open for internships & junior frontend opportunities.
+              Open for internships & junior frontend opportunities. 
             </p>
           </div>
         </div>
